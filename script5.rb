e991c8879814d6e4b67864454b87740db2f6ef39
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-15 10:57:20 +0300

    feat: Putting the Form in Formatter -Printing the Output

---
+++ script5.rb
@@ -0,0 +1,15 @@
+print "What's your first name? "
+first_name = gets.chomp
+puts "#{first_name}"
+
+print "What's your last name? "
+last_name = gets.chomp
+puts "#{last_name}"
+
+print "What city are you from? "
+city = gets.chomp
+puts "#{city}"
+
+print "What state or province are you from? "
+state = gets.chomp
+puts "#{state}"
